e0c4d4a4c383d08e765bd5190e0b3837d87506b4
Petar <petar.slunjski@abc-techgroup.com>
2020-05-15 16:42:42 +0200

    working on model

--- src/main/java/com/leads/model/CarBrand.java
+++ src/main/java/com/leads/model/CarBrand.java
@@ -1,6 +1,47 @@
 package com.leads.model;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
+/**
+ * Created by Petar
+ */
+@Entity
+@Table(name = "car_brand")
 public class CarBrand implements Serializable {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "name")
+    private String Name;
+
+    public CarBrand() {
+    }
+
+    public CarBrand(Integer id) {
+        this.id = id;
+    }
+
+    public CarBrand(Integer id, String name) {
+        this.id = id;
+        Name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
 }
--- src/main/java/com/leads/model/CarModel.java
+++ src/main/java/com/leads/model/CarModel.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
-@Table(name = "CarModel")
+@Table(name = "car_model")
 public class CarModel implements Serializable {
 
     @Id
